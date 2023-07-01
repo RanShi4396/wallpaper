e8369f099e90a9fb8fe0bcea39799c5ff07d3c54
Ran Shi <ranshi4396@outlook.com>
2023-07-01 03:05:01 -0400

    first commit

---
+++ resolution_change.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+xrandr --output eDP-1 --rate 85 --mode 1920x1200
+xinput set-prop 12 375 1 1 1 2 1 3
+feh --bg-fill  --no-fehbg ~/Pictures/wallpaper/60340_nichijou.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/shinkai_foot_anime.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/sunshine_room.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/landing_plane.png
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/aircraft_carrier.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/tadiao.png
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/jiayouzhan.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/railway.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/launch_1.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/launch_2.png
+
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/dnf_yuansu.jpg#
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/dnf_modao.jpg
+#feh --bg-fill --no-fehbg ~/Pictures/wallpaper/dnf_modao_1.jpg
+
+# randomly switch wallpaper, independent for each monitor
+#while true; do
+#  feh --bg-fill -no-fehbg --randomize ~/Pictures/wallpaper
+#  sleep 30
+#done
+
